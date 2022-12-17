13de5e126f3e50147bc91b81eecb89b35353a6bb
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-17 17:20:19 +0000

    refactor: uplift state from Board to TicTacToe component
    This prepares us for adding move history traversion.

--- src/features/TicTacToe/Board.tsx
+++ src/features/TicTacToe/Board.tsx
@@ -2,65 +2,19 @@ import Square from './Square';
 import './Board.css';
 import { useState } from 'react';
 
-type Props = {};
-
-/**
- * Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
- * @param squares
- * @returns
- */
-const calculateWinner = (squares: Array<Number>) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+type Props = {
+  squares: Array<Number>;
+  onClick: (i: number) => void;
 };
 
-const Board = (props: Props) => {
-  const handleClick = (i: number) => {
-    const slicedSquares = squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    slicedSquares[i] = currentPlayer();
-    setSquares(slicedSquares);
-    togglePlayer();
-  };
-
-  const currentPlayer = () => (xIsNext ? 'X' : 'O');
-  const togglePlayer = () => setXIsNext(!xIsNext);
-
+const Board = ({ squares, onClick }: Props) => {
   const createSquare = (i: number) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
-  const [squares, setSquares] = useState(Array(9).fill(undefined));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const winner = calculateWinner(squares);
-  let status = undefined;
-
-  if (winner) status = 'Winner: ' + winner;
-  else status = 'Next player: ' + (xIsNext ? 'X' : 'O');
 
   return (
     <div>
       <div>
-        <div className='status'>{status}</div>
         <div className='board-row'>
           {createSquare(0)}
           {createSquare(1)}
--- src/features/TicTacToe/TicTacToe.tsx
+++ src/features/TicTacToe/TicTacToe.tsx
@@ -1,18 +1,80 @@
+import { useState } from 'react';
 import Board from './Board';
 import './TicTacToe.css';
 
 type Props = {};
 
+/**
+ * Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
+ * @param squares
+ * @returns
+ */
+const calculateWinner = (squares: Array<Number>) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 const TicTacToe = (props: Props) => {
+  const [history, setHistory] = useState([
+    { squares: Array(9).fill(undefined) },
+  ]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const handleClick = (i: number) => {
+    const squares = current.squares.slice();
+
+    // Check if a player has won already or the square is already filled
+    if (calculateWinner(current.squares) || current.squares[i]) {
+      return;
+    }
+
+    squares[i] = currentPlayer();
+    setHistory(history.concat([{ squares: squares }]));
+    togglePlayer();
+  };
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const currentPlayer = () => (xIsNext ? 'X' : 'O');
+  const togglePlayer = () => setXIsNext(!xIsNext);
+
+  const current = history[history.length - 1];
+
+  const winner = calculateWinner(current.squares);
+
+  let status = undefined;
+
+  if (winner) status = 'Winner: ' + winner;
+  else status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+
   return (
     <div>
       <h1>Tic Tac Toe</h1>
       <div className='game'>
         <div className='game-board'>
-          <Board />
+          <Board squares={current.squares} onClick={(i) => handleClick(i)} />
         </div>
         <div className='game-info'>
-          <div>{/* status */}</div>
+          <div className='status'>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
